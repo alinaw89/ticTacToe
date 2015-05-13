f958f5fbf5c2f5add0867f9701f039f89191e350
alinaw89 <ali.naw89@gmail.com>
2015-05-13 18:57:00 -0400

    adding comments to the js page

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -6,39 +6,36 @@ var playerNames=["Sir Meowsworth", "Sir Puppington"];
 
 var count=0;
 
-$(document).ready(function(){
-  $("button").click(function(){
-     resetGame();
-  })
-  $(".square").click(function(){ //game loop
+$(document).ready(function(){ //1.event that fires when the html has been loaded
+  $("button").click(resetGame);//variable that points to a function
 
-    if($(this).html() === ""){
+  //beginning of square click handler
+  //game loop
+  $(".square").click(function(){ //Setting a click handler on every element that has class (.square)
 
+    if($(this).html() === ""){ //the "this" represents the one element that is being clicked; see if the current elements of html is empty, if they are set that equal to an empty string
+      $(this).html(players[player]); //inserts the img tag; selecting one of the two images to be displayed in the square
 
-    var playerArray=[]; //reset the board array
-     $(this).html(players[player]);
-     $(".square").each(function(){
-      playerArray.push($(this).html());
-     })
-     console.log(playerArray);
-    if (getWinner(playerArray)){
-      $("h3").html("Winner: " + playerNames[player]);
-      $("body").css("background-image", "url('http://filme-carti.ro/wp-content/uploads/2014/05/urme-de-laba.jpg')");
-      setBanner(player);
+      var playerArray=[];             //resets the board array
+      $(".square").each(function(){   //re-load the board array based on the current state of the HTML
+        playerArray.push($(this).html()); //so, if there are img tags it will place itself in that array, it returns an empty string instead
+      }) //
 
-
+      if (getWinner(playerArray)){
+        $("h3").html("Winner: " + playerNames[player]);
+        $("body").css("background-image", "url('http://filme-carti.ro/wp-content/uploads/2014/05/urme-de-laba.jpg')");
+        setBanner(player);
       //if there is no winner and if the counter is to 8
-    } else if (!getWinner(playerArray) && count === 8){
-      $("h3").html("DRAW! No winner.");
-    }else{
-      $(".turn").html(players[1-player]); //switch img
+      } else if (!getWinner(playerArray) && count === 8){
+        $("h3").html("Game Over! No winner.");
+      } else{
+        $(".turn").html(players[1-player]); //switch img
+      }
+      togglePlayer();
+      count+=1;
     }
 
-       togglePlayer();
-       count+=1;
-}
-
-  });
+  }); // end of square click handler
 
   function togglePlayer(){ //accesses the global variable
     player = 1- player;
@@ -68,13 +65,13 @@ $(document).ready(function(){
     return ret;
   }
 
-  function resetGame(){
+  function resetGame(){ //this is executed when the button click event occurs
     $(".square").each(function(){
-     $(this).html("");
-   });
+      $(this).html("");
+    });
     $("body").css("background-image", "none");
     $("h3").html("");
-    count=0;
+    count = 0;
   }
 
 
