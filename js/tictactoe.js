4bcaa9ab30f61a7a3848c9d2473821d35cc43fc2
alinaw89 <ali.naw89@gmail.com>
2015-05-14 11:15:08 -0400

    fixes bug with tie players

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,74 +1,77 @@
 var player = 0;
 
-var players=['<img src="http://cache1.asset-cache.net/xt/887210-001.jpg?v=1&g=fs1%7C0%7CTSIR%7C72%7C101&s=1">', '<img src="http://media-cache-ec0.pinimg.com/236x/3e/b7/c5/3eb7c5bbbda15079ed8c1b9c9741d846.jpg">'];
+var players = ['<img src="http://cache1.asset-cache.net/xt/887210-001.jpg?v=1&g=fs1%7C0%7CTSIR%7C72%7C101&s=1">', '<img src="http://media-cache-ec0.pinimg.com/236x/3e/b7/c5/3eb7c5bbbda15079ed8c1b9c9741d846.jpg">'];
 
-var playerNames=["Sir Meowington", "Sir Puppington"];
+var playerNames = ["Sir Meowington", "Sir Puppington"];
 
 var count = 0;
 
 // Each element will have:
-//  a empty string if the corresponding square has NOT been selected.
-//  a cat image if a Cat was selected
+//  an empty string if the corresponding square has NOT been selected.
+//  a cat image if a cat was selected
 //  a dog image if a dog was selected
-var board = ['', '', '', '', '', '', '', '']; //created empty board to track what squares have been clicked clicked
+var board = ['', '', '', '', '', '', '', '']; // Created empty board to track what squares have been clicked clicked
 var boardIndex;
 var drawCat = true;
 var foundWinner = false;
 
+// Event that fires when the html has been loaded
+$(document).ready(function(){
+  // Button click event that points to a function called resetGame
+  $("button").click(resetGame);
 
-$(document).ready(function(){ //1.event that fires when the html has been loaded
-  $("button").click(resetGame);//variable that points to a function
+  // Beginning of square click handler and game loop
 
-  //beginning of square click handler
-  //game loop
-  $(".square").click(function(event){ //Setting a click handler on every element that has class (.square)
+  // Setting a click handler on every element that has class (.square)
+  $(".square").click(function(event){
 
-    if($(this).html() === ""){ //the "this" represents the one element that is being clicked; see if the current elements of html is empty, if they are set that equal to an empty string
-      $(this).html(players[player]); //inserts the img tag; selecting one of the two images to be displayed in the square
+    // The "this" represents the one element that is being clicked; see if the current elements of html is empty. If so, set that equal to an empty string
+    if($(this).html() === ""){
+      // Inserts the img tag on the element that is being clicked on; selecting either the cat or dog
+      $(this).html(players[player]);
 
-      // get the id of the element that was clicked
-      boardIndex = event.target.id; //give me the id of the element i clicked on, that will be a number that used as an index into the board array
+      // Gives the id of the element that was clicked on. That will be a number that is used as an index in the board array
+      boardIndex = event.target.id;
 
       if(drawCat){
-        // set the board item to 'cat' using the id from the square clicked
+        // Set the board item to 'cat' using the id from the square clicked
         board[boardIndex] = 'cat';
-        // is the cat a winner?
+        // Is the cat a winner?
         foundWinner = getWinner('cat');
-        // next time draw a dog
+        // Next time draw a dog
         drawCat = false; //drawcat is false so that next time we will draw a dog
       }else{
-        // set the board item to 'dog' using the id from the square clicked
+        // Set the board item to 'dog' using the id from the square clicked
         board[boardIndex] = 'dog';
-        // is the dog a winner?
+        // Is the dog a winner?
         foundWinner = getWinner('dog');
-        // next time you'll draw a cat
+        // Next time you'll draw a cat
         drawCat = true;
       }
 
       if (foundWinner){
         // Hey, either the cat or the dog won. So, show it's name
         $("h3").html("Winner: " + playerNames[player]);
-        // And show it's picture as a winner
+        // And show a background of paw prints indicating a winner
         $("body").css("background-image", "url('http://filme-carti.ro/wp-content/uploads/2014/05/urme-de-laba.jpg')");
         // Change banner to Meow or Woof
         setBanner(player);
-      //if there is no winner and if the counter is to 8
+        // If there is no winner and if the counter is to 8
       } else if (!foundWinner && count === 8){
         // No winner after 9 clicks, game over
         $("h3").html("Game Over! No winner.");
-        resetGame();
       } else{
         // No winner and game not over so switch image for next player (dog or cat)
-        $(".turn").html(players[1-player]); //switch img
+        $(".turn").html(players[1-player]);
       }
 
       togglePlayer();
       count += 1;
     }
 
-  }); // end of square click handler
+  }); // End of square click handler
 
-  function togglePlayer(){ //accesses the global variable
+  function togglePlayer(){
     player = 1- player;
   }
 
@@ -97,14 +100,20 @@ $(document).ready(function(){ //1.event that fires when the html has been loaded
     return result;
   }
 
-
-  function resetGame(){ //this is executed when the button click event occurs
+  // Game resents when button click event occurs
+  function resetGame(){
+    // Goes through each element that has the (.square) class
     $(".square").each(function(){
+      // Sets every element that has the square class to an empty string
       $(this).html("");
     });
-     $("body").css("background-image", "none");
+      // Resets background, winner html and title
+      $("body").css("background-image", "none");
     $("h3").html("");
+    $("#gametitle").html('<img class="pawprint" src="http://www.nashvillewraps.com/images/large_stkno/ppc.jpg"/> Tic Tac Toe <img class="pawprint" src="http://www.nashvillewraps.com/images/large_stkno/ppc.jpg"/>');
+    // Sets the counter back to 0
     count = 0;
+    // Sets the board array to empty strings
     board = ['', '', '', '', '', '', '', ''];
   }
 
@@ -113,7 +122,7 @@ $(document).ready(function(){ //1.event that fires when the html has been loaded
     if (player === 0){
       $(".changename").html("Meow").css("color", "red");
     } else{
-       $(".changename").html("Woof").css("color", "red");
+      $(".changename").html("Woof").css("color", "red");
     }
   }
 
