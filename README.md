75a88735c804cffea028370af218c2684bd16ca5
alinaw89 <ali.naw89@gmail.com>
2015-08-05 17:57:33 -0400

    fixes tabbing in html

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-# Tic Tac Toe
+Tic Tac Toe
+Game Link
 
-
-##Game Link
 https://alinaw89-tictactoe.firebaseapp.com
 
-
-## Overview
+Overview
 
 I rendered a tictactoe gameboard using HTML5, CSS, Javascript, Bootstrap 3.3.4 and jQuery 2.1.3 for a General Assembly project. In order to play, the user will load the game in their browser by clicking the game link or loading the index.html file. The user will then be able to play. The game will notify the user when a player has won. It will also display if there is a tie (no win). Should the user have any questions on the code, please refer to the comments in the index.html and tictactoe.js files.
 
-
-## Approach Taken:
+Approach Taken:
 
 User Story 1: User should see a tic tac toe board on the main page.
 
@@ -22,20 +19,11 @@ User Story 4: User should be able to win if user selects three Cat or Dog images
 
 User Story 5: Game should display a draw if there is no winner.
 
+Instructions
 
-## Instructions
-
-1. The player should see a tic tac toe game in the browser after loading the index.html file.
-1. The game automatically is set to start where Player#1 is the Cat and Player #2 is the Dog.
-2. Player#1 and Player#2 alternate turns by selecting one square at a time on the game board.
-3. Once a player selects three squares in a row (horizontally, vertically or diagonally), that player will win the game and a prompt will show up indicating who the winner is. A scoreboard will also reflect the current amount of wins between both players.
-4. If no player is able to win, the game will be over.
-5. Players can reset the game at any given point.
-
-
-
-
-
-
-
-
+The player should see a tic tac toe game in the browser after loading the index.html file.
+The game automatically is set to start where Player#1 is the Cat and Player #2 is the Dog.
+Player#1 and Player#2 alternate turns by selecting one square at a time on the game board.
+Once a player selects three squares in a row (horizontally, vertically or diagonally), that player will win the game and a prompt will show up indicating who the winner is. A scoreboard will also reflect the current amount of wins between both players.
+If no player is able to win, the game will be over.
+Players can reset the game at any given point.
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -6,7 +6,7 @@ var playerNames = ["Sir Meowington", "Sir Puppington"];
 
 var count = 0;
 
-var scores=[0,0];
+var scores = [0, 0];
 
 // Each element will have:
 //  an empty string if the corresponding square has NOT been selected.
@@ -20,7 +20,7 @@ var drawCat = true;
 var foundWinner = false;
 
 // Event that fires when the html has been loaded
-$(document).ready(function(){
+$(document).ready(function() {
   // Button click event that points to a function called resetGame
   // Did not use resetGame() because that would load immediately after the html loads
   $("button").click(resetGame);
@@ -28,17 +28,17 @@ $(document).ready(function(){
   // Beginning of square click handler and game loop
 
   // Setting a click handler on every element that has class (.square)
-  $(".square").click(function(event){
+  $(".square").click(function(event) {
 
     // The "this" represents the one element that is being clicked; see if the current elements of html is empty. If so, set that equal to an empty string
-    if($(this).html() === ""){
+    if ($(this).html() === "") {
       // Inserts the img tag on the element that is being clicked on; selecting either the cat or dog
       $(this).html(players[player]);
 
       // Gives the id of the element that was clicked on. That will be a number that is used as an index in the board array
       boardIndex = event.target.id;
 
-      if(drawCat){
+      if (drawCat) {
         // Set the board item to 'cat' using the id from the square clicked
         board[boardIndex] = 'cat';
         // Is the cat a winner?
@@ -54,7 +54,7 @@ $(document).ready(function(){
         drawCat = true;
       }
 
-      if(foundWinner){
+      if (foundWinner) {
         // Hey, either the cat or the dog won. So, show it's name
         $("h3").html("Winner: " + playerNames[player]);
         addToScoreboard(player);
@@ -65,12 +65,12 @@ $(document).ready(function(){
         // Change banner to Meow or Woof
         setBanner(player);
         // If there is no winner and if the counter is to 8
-      } else if (!foundWinner && count === 8){
+      } else if (!foundWinner && count === 8) {
         // No winner after 9 clicks, game over
         $("h3").html("It's a tie! Game Over!");
-      } else{
+      } else {
         // No winner and game not over so switch image for next player (dog or cat)
-        $(".turn").html(players[1-player]);
+        $(".turn").html(players[1 - player]);
       }
 
       togglePlayer();
@@ -79,43 +79,43 @@ $(document).ready(function(){
 
   }); // End of square click handler
 
-  function togglePlayer(){
-    player = 1- player;
+  function togglePlayer() {
+    player = 1 - player;
   }
 
 
-  function getWinner(catOrDog){
+  function getWinner(catOrDog) {
     var val = catOrDog;
     var result = false;
 
-    if(board[0] === val && board[1] === val && board[2] === val){
+    if (board[0] === val && board[1] === val && board[2] === val) {
       result = true;
-    }else if(board[3] === val && board[4] === val && board[5] === val){
+    } else if (board[3] === val && board[4] === val && board[5] === val) {
       result = true;
-    }else if(board[6] === val && board[7] === val && board[8] === val){
+    } else if (board[6] === val && board[7] === val && board[8] === val) {
       result = true;
-    }else if(board[0] === val && board[3] === val && board[6] === val){
-     result = true;
-    }else if(board[1] === val && board[4] === val && board[7] === val){
-     result = true;
-    }else if(board[2] === val && board[5] === val && board[8] === val){
-     result = true;
-    }else if(board[0] === val && board[4] === val && board[8] === val){
-     result = true;
-    }else if(board[2] === val && board[4] === val && board[6] === val){
-     result = true;
+    } else if (board[0] === val && board[3] === val && board[6] === val) {
+      result = true;
+    } else if (board[1] === val && board[4] === val && board[7] === val) {
+      result = true;
+    } else if (board[2] === val && board[5] === val && board[8] === val) {
+      result = true;
+    } else if (board[0] === val && board[4] === val && board[8] === val) {
+      result = true;
+    } else if (board[2] === val && board[4] === val && board[6] === val) {
+      result = true;
     }
     return result;
   }
 
-  // Game resents when button click event occurs
-  function resetGame(){
+  // Game resets when button click event occurs
+  function resetGame() {
     // Goes through each element that has the (.square) class
-    $(".square").each(function(){
+    $(".square").each(function() {
       // Sets every element that has the square class to an empty string
       $(this).html("");
     });
-      // Resets background, winner html and title
+    // Resets background, winner html and title
     $("body").css("background-image", "none");
     $("h3").html("");
     $("#gametitle").html('<img class="pawprint" src="http://www.nashvillewraps.com/images/large_stkno/ppc.jpg"/>Tic Tac <span class="changename">Toe</span><img class="pawprint" src="http://www.nashvillewraps.com/images/large_stkno/ppc.jpg"/>');
@@ -126,17 +126,20 @@ $(document).ready(function(){
   }
 
 
-  function setBanner(player){
-    if (player === 0){
+  function setBanner(player) {
+    if (player === 0) {
       $(".changename").html("Meow").css("color", "red");
     } else {
       $(".changename").html("Woof").css("color", "red");
     }
   }
 
-  function addToScoreboard(player){
-    scores[player]+=1;
+  function addToScoreboard(player) {
+    scores[player] += 1;
   }
 
 });
 
+//Things to improve:
+//variables
+//count to clickCount
